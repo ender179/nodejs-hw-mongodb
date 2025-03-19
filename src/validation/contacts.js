e7704f5e930a579d98434befcd3e9dc2ec936654
ender179 <kirilukvlad91@gmail.com>
2025-03-20 00:08:46 +0200

    git push

--- src/validation/contacts.js
+++ src/validation/contacts.js
@@ -1,13 +1,15 @@
 import Joi from 'joi';  
 
+// Схема валідації для створення контакту  
 export const createContactsSchema = Joi.object({  
-    name: Joi.string().min(1).required(),   
-    phoneNumber: Joi.string().pattern(/^[0-9]+$/).min(10).max(15).required(),   
-    email: Joi.string().email().required(),   
+  name: Joi.string().min(1).required(),  
+  phoneNumber: Joi.string().pattern(/^[0-9]+$/).min(10).max(15).required(),  
+  email: Joi.string().email().required(),  
 });  
 
+// Схема валідації для оновлення контакту  
 export const updateContactsSchema = Joi.object({  
-    name: Joi.string().min(1),  
-    phoneNumber: Joi.string().pattern(/^[0-9]+$/).min(10).max(15),   
-    email: Joi.string().email(),    
+  name: Joi.string().min(1),  
+  phoneNumber: Joi.string().pattern(/^[0-9]+$/).min(10).max(15),  
+  email: Joi.string().email(),  
 });  
