94f99cc9b285d0417ad932c07e88f9d60380610e
ender179 <kirilukvlad91@gmail.com>
2025-03-20 20:22:59 +0200

    git push

--- src/validation/contacts.js
+++ src/validation/contacts.js
@@ -5,8 +5,8 @@ export const createContactsSchema = Joi.object({
     name: Joi.string().min(1).required(),  
     phoneNumber: Joi.string().pattern(/^[0-9]+$/).min(10).max(15).required(),  
     email: Joi.string().email().required(),  
-    isFavourite: Joi.boolean().default(false),  // Додаємо isFavourite  
-    contactType: Joi.string().valid('personal', 'professional').required()  // Додаємо contactType  
+    isFavourite: Joi.boolean().default(false),  
+    contactType: Joi.string().valid('personal', 'professional').required()    
 });  
 
 // Схема валідації для оновлення контакту  
@@ -14,6 +14,6 @@ export const updateContactsSchema = Joi.object({
     name: Joi.string().min(1),  
     phoneNumber: Joi.string().pattern(/^[0-9]+$/).min(10).max(15),  
     email: Joi.string().email(),  
-    isFavourite: Joi.boolean(),  // Додаємо isFavourite  
-    contactType: Joi.string().valid('personal', 'professional')  // Додаємо contactType  
+    isFavourite: Joi.boolean(),  
+    contactType: Joi.string().valid('personal', 'professional')    
 });  
